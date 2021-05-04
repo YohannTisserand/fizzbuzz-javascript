b63e3b56d581407ba95f830a4903ff3e3ccc3fc8
YohannTisserand <tisserand.yohann@gmail.com>
2021-05-04 18:41:35 +0100

    Fizzbuzz done

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,13 +1,30 @@
 class FizzBuzz {
   isDivisibleByThree(number) {
-    return (number % 3 === 0);
+    return this._isDivisibleBy(number, 3);
+  }
+
+  says(number) {
+    if (this.isDivisibleByFifteen(number)) {
+      return 'FizzBuzz'
+    }
+    if (this.isDivisibleByThree(number)) {
+      return 'Fizz'
+    }
+    if (this.isDivisibleByFive(number)) {
+      return 'Buzz'
+    }
+    return number;
   }
 
   isDivisibleByFive(number) {
-    return (number % 5 === 0);
+    return this._isDivisibleBy(number, 5);
   }
 
   isDivisibleByFifteen(number) {
-    return (number % 15 == 0);
+    return this._isDivisibleBy(number, 15);
+  }
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
   }
 }
